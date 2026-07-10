3a1e7a5a485f7207e8168562ecda79719146b933
nmraya <nraya06@gmail.com>
2026-07-10 13:05:49 -0700

    More fixes to removeWorkout function

--- WorkoutTracker.cpp
+++ WorkoutTracker.cpp
@@ -124,36 +124,37 @@ void removeWorkout(vector<Workout> &Workouts){
     while (matchingWorkout == false){
         cout << "Type workout to remove: ";
         cin >> removedWorkoutname;
-        cout << "Type workout date to remove: ";
-        cin >> removedWorkoutdate;
+        //want to make sure the workout name is valid before checking date
         for (int i = 0; i < Workouts.size(); i++){
-        //go through each element in vector
-            if (removedWorkoutname == Workouts[i].name){ //need to account also for date this only checks if workout name matched need to check if date matches too
-                if(removedWorkoutdate == Workouts[i].date){
-                    Workouts.erase(Workouts.begin() + i);//why do we not need to erase the .name & .date why can we just remove the workout as posiition i?
-                    //is it because the name and date are both stored at the index i so we just need to remove that specific index
-                    //if thats the case why cant we just check if removedWorkoutname == Workouts[i] why do we need Workouts[i].name
-                    //the reason we can do the erase is because its erasing both the objects stored at that position i so its fine
-                    //but however we cant compare removedWorkoutdate and Workouts[i] because of types, removedWorkoutname is a string and 
-                    //Workouts[i] is the objectso we cant compare those two its like trying to compare a string == Workout object, so we need to get more specific
-                    //which is like string == string 
-                    //Workouts[i] is one specific Workout object stored at index i
-                    //Workouts is the vector that stores many Workout objects
-                    //Workout is the class/type
-                    
-                    matchingWorkout = true; 
-                }
-                
+            //loops through entire vector
+            if (removedWorkoutname == Workouts[i].name){
+                //bool validDate = false;
+                //now that workout name is checked to be valid, check if date valid
+                //also add in while loop later to keep checking if you dont want return to top
+                //while(validDate == false){
+                //}
+                cout << "Type workout date to remove: ";
+                cin >> removedWorkoutdate;
             }
+            if(removedWorkoutdate == Workouts[i].date){
+                Workouts.erase(Workouts.begin() + i);
+                matchingWorkout = true;
+                //validDate = true; 
+            }
+
         }
-        if (matchingWorkout == false){
+        cout << "Invalid workout name, please retry " << endl;
+
+
+
+    }
+
+
+    if (matchingWorkout == false){
             cout << "Please enter valid workout" << endl;
             //if no match was found, matchingWorkout stays = false and then we go
             //to top of while loop again and repeat until we find a match
         }
-
-        
-    }
 
 
     cout << "----------" << endl;
